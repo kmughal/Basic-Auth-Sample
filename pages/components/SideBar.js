cd4a4b34a67c805322b9502511a619276d6248f3
kmughal <khurram@Khurrams-MBP.home>
2020-11-15 15:52:40 +0000

    Change SideBar component
    - now show a message if there is not project for a new user

--- pages/components/SideBar.js
+++ pages/components/SideBar.js
@@ -10,12 +10,13 @@ export default function SideBar({ username }) {
         .then(setprojects)
         .catch(console.error)
     }, [])
-    
+
     return (
       <>
         <div className="main-page__side-menu">
           <ProjectSetupModal username={username} />
-          {projects && projects.length && (
+          {(!projects || projects.length === 0) && (<p>No channel added yet!</p>)}
+          {projects && projects.length > 0 && (
             <ul className="projects-container">
               {projects.map((p) => (
                 <li key={p._id}>{p.projectName}</li>
