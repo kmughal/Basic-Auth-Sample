97f0ca4cf48ba735c94b0e980e2a4dba2f252964
kmughal <khurram@Khurrams-MBP.home>
2020-11-15 15:52:55 +0000

    Change SignIn component
    - just added a static content

--- pages/components/SignIn.js
+++ pages/components/SignIn.js
@@ -5,6 +5,7 @@ export default function SignIn() {
     <section className="signin-container">
       <p>
         Not signed in <button onClick={signIn}>Click here to signin</button>
+        Or if you don't have an account then <a href="/register">click here</a> to register
       </p>
     </section>
   )
