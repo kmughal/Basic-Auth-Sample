8f6aa5d688a962d9aa51406e4837c7871e6f8227
Khurram <KhurramShahzad@tfl.gov.uk>
2020-11-16 10:55:25 +0000

    add Long polling but this has to change

--- pages/components/MainPage.js
+++ pages/components/MainPage.js
@@ -1,21 +1,35 @@
-import React from "react"
-import SideBar from "./SideBar"
-import MainBody from "./MainBody"
+import React from "react";
+import SideBar from "./SideBar";
+import MainBody from "./MainBody";
 
-export default function MainPage({username}) {
-  const [selectedChannel, setSelectedChannel] = React.useState(null)
-  const [messages, setMessages] = React.useState(null)
+export default function MainPage({ username }) {
+  const [selectedChannel, setSelectedChannel] = React.useState(null);
+  const [messages, setMessages] = React.useState(null);
   function getMessagesByChannel(id) {
-    fetch("/api/messages?channelId=" + (id ?? selectedChannel) )
+    if (!(id ?? selectedChannel)) return;
+    fetch("/api/messages?channelId=" + (id ?? selectedChannel))
       .then((r) => r.json())
       .then(setMessages)
-      .catch(console.error)
+      .catch(console.error);
   }
+
+  // Long polling this needs to change.
+  // setTimeout(() => {
+  //   getMessagesByChannel();
+  // }, 3000);
 
   return (
     <section className="main-page">
-      <SideBar setSelectedChannel={setSelectedChannel} getMessagesByChannel={getMessagesByChannel}/>
-      <MainBody selectedChannel={selectedChannel} messages={messages} getMessagesByChannel={getMessagesByChannel} username={username}></MainBody>
+      <SideBar
+        setSelectedChannel={setSelectedChannel}
+        getMessagesByChannel={getMessagesByChannel}
+      />
+      <MainBody
+        selectedChannel={selectedChannel}
+        messages={messages}
+        getMessagesByChannel={getMessagesByChannel}
+        username={username}
+      ></MainBody>
     </section>
-  )
+  );
 }
