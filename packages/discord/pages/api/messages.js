e2e00a1f016238244350fcfcd6b06d3b39f57a17
Khurram <KhurramShahzad@tfl.gov.uk>
2020-11-17 13:28:43 +0000

    add ability to remove messages still wip!

--- packages/discord/pages/api/messages.js
+++ packages/discord/pages/api/messages.js
@@ -1,46 +1,60 @@
-import { MessageModel } from "./db/Models"
-import startMongoose from "./db"
-import { getSession } from "next-auth/client"
-import MessagesSchema from "./db/Schemas/MessagesSchema"
+import { MessageModel } from "./db/Models";
+import startMongoose from "./db";
+import { getSession } from "next-auth/client";
 
 export default async function (req, res) {
-  await startMongoose()
- 
-
+  await startMongoose();
+  const { user } = await getSession({ req });
+  if (!user || !user._id) {
+    res.status(404);
+    return;
+  }
   if (req.method === "POST") {
-    const { user } = await getSession({ req })
-    if (!user || !user._id) {
-      res.status(404)
-      return
-    }
-    const { channelId, message } = req.body
+    const { channelId, message } = req.body;
 
     if (!channelId || !message) {
-      res.status(404)
-      return
+      res.status(404);
+      return;
     }
-    const instance = new MessageModel()
-    instance.user = user._id
-    instance.channel = channelId
-    instance.message = message
-    instance.save()
-    res.status(302).send("message added to the selected channel")
-    return
-  }
-
-  if (req.method == "GET") {
-    const { channelId } = req.query
+    const instance = new MessageModel();
+    instance.user = user._id;
+    instance.channel = channelId;
+    instance.message = message;
+    instance.save();
+    res.status(302).send("message added to the selected channel");
+    return;
+  } else if (req.method == "GET") {
+    const { channelId } = req.query;
 
     if (!channelId) {
-      res.status(404).send("Bad request")
-      return
+      res.status(404).send("Bad request");
+      return;
     }
 
     const result = await MessageModel.find({
       channel: channelId,
-    }).populate("user", "username")
+    }).populate("user", "username");
 
+    res.json(result ?? []);
+  } else if (req.method == "DELETE") {
+    const { messageId } = req.body;
 
-    res.json(result ?? [])
+    if (!messageId) {
+      res.status(404).send("Bad request");
+      return;
+    }
+
+    const existingMessage = await MessageModel.find({ _id: messageId });
+    const isOwner = user._id !== existingMessage.user;
+    if (!isOwner) {
+      res.status(404).send("Bad request");
+      return;
+    }
+
+    await MessageModel.deleteOne({
+      _id: messageId,
+    });
+
+    res.status(204).send("message deleted" + messageId);
   }
 }
--- packages/discord/pages/components/Messages.js
+++ packages/discord/pages/components/Messages.js
@@ -1,18 +1,71 @@
+import React from "react";
+
 export default function Messages({ messages, username }) {
+  const editorToolBar = React.useRef(null);
+
+  const showEditorToolBar = (m, { target }) => {
+    editorToolBar.current.visibility = "hidden";
+    target.appendChild(editorToolBar.current);
+    editorToolBar.current.data = m;
+    editorToolBar.current.data.isOwn = !target.classList.contains(
+      "message-container__not-author"
+    );
+    editorToolBar.current.visibility = "visible";
+  };
+
+  React.useEffect(() => {
+    editorToolBar.current.visibility = "hidden";
+  }, []);
+
   return (
-    <ul className="message-container">
-      {messages.map((m) => (
-        <li
-          className={
-            username === m.user.username
-              ? "message-container__author"
-              : "message-container__not-author"
-          }
-          key={m._id}
-        >
-          {m.user.username} say :{m.message}
-        </li>
-      ))}
-    </ul>
-  )
+    <section>
+      <EditorToolBar editorToolBar={editorToolBar} />
+      <ul className="message-container">
+        {messages.map((m) => (
+          <li
+            className={
+              username === m.user.username
+                ? "message-container__author"
+                : "message-container__not-author"
+            }
+            key={m._id}
+            onMouseOver={(e) => showEditorToolBar(m, e)}
+          >
+            {m.user.username} say :{m.message}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function EditorToolBar({ editorToolBar }) {
+  const deleteMessageHandler = (_) => {
+    const body = JSON.stringify({ messageId: editorToolBar.current.data._id });
+    fetch("/api/messages", {
+      method: "delete",
+      headers: {
+        "content-type": "application/json",
+      },
+      body,
+    })
+      .then(console.log)
+      .catch(console.trace);
+  };
+
+  return (
+    <section className="editor-toolbar" ref={editorToolBar}>
+      <ul>
+        <li>Edit</li>
+        {editorToolBar.current?.data?.isOwn && (
+          <li
+            className="editor-toolbar_action-delete"
+            onClick={deleteMessageHandler}
+          >
+            Delete
+          </li>
+        )}
+      </ul>
+    </section>
+  );
 }
